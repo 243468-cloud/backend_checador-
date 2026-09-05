a337ceb77799c83dc021883b7b3ea6bc43f38843
243468-cloud <243468@ip.upchiapas.edu.mx>
2026-09-05 09:11:51 -0600

    fix: add missing import for AuthenticationPrincipal

--- src/main/java/com/checador/controller/AuthController.java
+++ src/main/java/com/checador/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
